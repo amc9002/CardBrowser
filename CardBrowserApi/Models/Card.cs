8229bfd3e39b5bf9eb2f940dd779dcfacb722732
Andrej Cieraskou <9002amc@gmail.com>
2022-10-13 23:19:34 +0300

    Some classes names changed, test card file added, json from test file deserialized

---
+++ CardBrowserApi/Models/Card.cs
@@ -0,0 +1,12 @@
+namespace CardBrowserApi.Models
+{
+    public class Card
+    {
+
+        //public int Id { get; private set; }
+        public string? Name { get; set; }
+        public string? BitImg { get; set; }
+
+    }
+
+}
