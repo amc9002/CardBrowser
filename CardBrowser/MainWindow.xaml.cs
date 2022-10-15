8f8b6675474a8f302ed34a10cdc4711b01cfff31
Andrej Cieraskou <9002amc@gmail.com>
2022-10-15 15:42:46 +0300

    API gets data from a json and graphic files

--- CardBrowser/MainWindow.xaml.cs
+++ CardBrowser/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace CardBrowser
 {
@@ -23,6 +24,15 @@ namespace CardBrowser
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+            {
+                lstNames.Items.Add(txtName.Text);
+                txtName.Clear();
+            }
         }
     }
 }
---
+++ CardBrowserApi/Controllers/CardController.cs
@@ -0,0 +1,51 @@
+using CardBrowserApi.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Newtonsoft.Json;
+
+using static System.Net.Mime.MediaTypeNames;
+using System.Text;
+
+namespace CardBrowserApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CardController : ControllerBase
+    {
+        private readonly ILogger<CardController> _logger;
+
+        public CardController(ILogger<CardController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<Card>? Get()
+        {
+            using (StreamReader r = new("Data/cards.json"))
+            {
+                string json = r.ReadToEnd();
+
+                List<Card>? cards = JsonConvert.DeserializeObject<List<Card>>(json);
+                if (cards != null)
+                {
+                    foreach (var card in cards)
+                    {
+                        string? fileName = "Data/Img/" + card.FileName;
+                        if (fileName != null)
+                        {
+                            using StreamReader reader = new(fileName);
+                            byte[] byteImg = Encoding.ASCII.GetBytes(reader.ReadToEnd());
+                            card.Img = byteImg;
+                        }
+                        else _logger.LogError("File doesn't exist");
+                    }
+                }
+                else _logger.LogError("No cards file");
+
+                return cards;
+            }
+        }
+    }
+}
